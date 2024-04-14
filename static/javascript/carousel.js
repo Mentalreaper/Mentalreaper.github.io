34b10bb34e79bd897cba94e0bbfe327d952862dd
Alexander JD Conn <work@alexanderconn.com>
2024-04-14 20:33:18 +0100

    quick change

--- static/javascript/carousel.js
+++ static/javascript/carousel.js
@@ -1,4 +1,3 @@
-const storageKey = 'theme-preference';
 const slides = document.querySelectorAll('.carousel-slide');
 let currentSlide = 0;
 
@@ -27,51 +26,6 @@ document.querySelectorAll('.arrow').forEach((arrow) => {
   });
 });
 
-document.body.classList.toggle('dark-mode');
-
-document.getElementById('theme-toggle').addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
-
-const getColorPreference = () => {
-  if (localStorage.getItem(storageKey)) {
-    return localStorage.getItem(storageKey);
-  }
-  return window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'dark'
-    : 'light';
-};
-
-const theme = {
-  value: getColorPreference(),
-};
-
-const reflectPreference = () => {
-  document.firstElementChild
-    .setAttribute('data-theme', theme.value);
-
-  document
-    .querySelector('#theme-toggle')
-    ?.setAttribute('aria-label', theme.value);
-};
-
-const setPreference = () => {
-  localStorage.setItem(storageKey, theme.value);
-  reflectPreference();
-};
-
-const onClick = () => {
-  // flip current value
-  theme.value = theme.value === 'light'
-    ? 'dark'
-    : 'light';
-
-  setPreference();
-};
-
-// set early so no page flashes / CSS is made aware
-reflectPreference();
-
 window.onload = function onStart() {
   window.addEventListener('scroll', function onEventListenerAdded() {
     if (window.scrollY > 100) {
@@ -81,24 +35,11 @@ window.onload = function onStart() {
     }
   });
 
-  reflectPreference();
-
   const menuButton = this.document.querySelector('.hamburger');
   const mobileMenu = this.document.querySelector('.mobile-nav');
-  const themeToggle = this.document.querySelector('#theme-toggle');
 
   menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('is-active');
     mobileMenu.classList.toggle('is-active');
   });
-
-  themeToggle.addEventListener('click', onClick);
 };
-
-// sync with system changes
-window
-  .matchMedia('(prefers-color-scheme: dark)')
-  .addEventListener('change', ({ matches: isDark }) => {
-    theme.value = isDark ? 'dark' : 'light';
-    setPreference();
-  });
